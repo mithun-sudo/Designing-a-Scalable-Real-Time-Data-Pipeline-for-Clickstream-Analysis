fef7e093e5d36cc130502358e11494524735b484
mithun-sudo <128903122+mithun-sudo@users.noreply.github.com>
2023-05-20 10:24:41 +0530

    Update Aggregation_with_spark.py

--- Aggregation_with_spark.py
+++ Aggregation_with_spark.py
@@ -17,9 +17,12 @@ df.createOrReplaceTempView("table")
 """Using in-built function datediff to find the time spent in seconds and later converting it to minutes."""
 df = spark.sql("select *, round(datediff(second, t1, t2) / 60, 2) as minutes_spent from table")
 
-"""Grouping the records by country and url, performing aggregation functions - count, countDistinct and average on
+"""Adding a column event_date so that end users can apply filter based on date"""
+df = df.withColumn("event_date", date_format(col("t1"), "MM-dd-yyyy"))
+
+"""Grouping the records by country, event_date and url, performing aggregation functions - count, countDistinct and average on
 click_event_id, user_id, minutes_spent respectively to find click counts, unique users count and minutes spent."""
-df = df.groupBy("country", "url").agg(count("click_event_id").alias("click_count"), avg("minutes_spent").alias("average_minutes_spent"), countDistinct("user_id").alias("unique_users_count"))
+df = df.groupBy("country", "url", "event_date").agg(count("click_event_id").alias("click_count"), avg("minutes_spent").alias("average_minutes_spent"), countDistinct("user_id").alias("unique_users_count"))
 
 """"Loading the dataframe to elastic search server hosted on localhost:9200."""
 df.write.format('org.elasticsearch.spark.sql').option('es.nodes', 'localhost').option('es.port', 9200).option('es.resource', '%s/%s' % ('test', 'data')).save()
